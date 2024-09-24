9bfa04fc9998804fcc4288046deea89bfff3d5da
kenny <kennysekhon@kennys-Mac-Pro-4.local>
2024-09-23 17:12:26 -0700

    Updated server.js to fix server configuration

--- server.js
+++ server.js
@@ -25,9 +25,6 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   port: process.env.DB_PORT,
-  ssl: {
-    rejectUnauthorized: false,
-  },
 });
 
 // Log a message when the connection pool is created
